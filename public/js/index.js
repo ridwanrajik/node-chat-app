d1f3bccae8bf8abcac5f73dd221637b6fcff21d4
Ridwan <ridwanraji4@gmail.com>
2022-09-07 12:08:00 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+let socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        to: 'jen',
+        text: 'Hey, This is Andrew'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (Message) {
+    console.log('New Message', Message);
+});
--- server/server.js
+++ server/server.js
@@ -9,11 +9,22 @@ const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
+// let io = socketIO(http.createServer(express()));
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Ridwan',
+        text: 'Hey, What is going on',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('Create Message', newMessage);
+    });
 
     socket.on('disconnect', () => {
         console.log('user was disconnected');
