1307b8165fde76c59d544a1ec79f7e45b0c05217
Ridwan <ridwanraji4@gmail.com>
2022-09-14 23:24:15 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child')
+    // Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -23,6 +39,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);  
+    scrollToBottom();
     
     // let formattedTime = moment(message.createdAt).format('h:mm a');
     // let li = jQuery('<li></li>');
@@ -41,6 +58,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // let formattedTime = moment(message.createdAt).format('h:mm a');
     // let li = jQuery('<li></li>');
