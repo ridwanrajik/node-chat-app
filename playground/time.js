ceec6012b9468608f9e9c33d92a94dd22aeb69e9
Ridwan <ridwanraji4@gmail.com>
2022-09-14 14:46:31 +0100

    Add mustache.js for message templates

--- playground/time.js
+++ playground/time.js
@@ -1,6 +1,10 @@
 // Exploring time in JavaScript
+
+// Note that the term timestamp is a number that corresponds to the time in seconds(in unix)/ milliseconds(in JavaScript) that is relative to a certain moment in history reffered to as the UNIX APIC which is January 1st 1970 at midnight( i.e 00:00:00 am). A timestamp of 0 corresponds the stated date and time perfectly, and a positive timestamp is relative to the date in the future, while a negative timestamp is relative to the date in the past.
 let date1 = new Date();
 console.log(date1); // prints 2022-09-12T20:28:59.457Z to the console. It prints today's date in that format
+let timestamp1 = new Date().getTime(); // This creates the timestamp of the current time and store it in timestamp1 variable
+console.log(timestamp1); // prints the timestamp of the current time to the console
 let months = ['Jan','Feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 console.log(date1.getMonth()) //prints 8 to the console which denotes our current month, september. Note that getMonth() returns numbers from 0 -11, which denotes Jan - Dec.
 console.log(months[date1.getMonth()]) // prints sep to the console
@@ -18,3 +22,11 @@ console.log(date2.format('MMM Do, YYYY')) // prints Sep 12th, 2023. note that th
 date2.add(100, 'years').subtract(9, 'months'); //add 100 year to this current year and subtract 9 months from the current month
 console.log(date2.format('MMM Do, YYYY')) // prints Dec 12th, 2122 to the console
 console.log(date2.format('hh:mm a')) // prints 11:04 pm to the console
+// note that on line 14 above, moment() returns the current time by using the current time stamp by default,
+// but when we pass when we pass in a time stamp as an argument into the moment() function, 
+// it returns the time of the time stamp passed as argument. see example below:
+let createdAt = 1234; // this is the time stamp something was created
+let date3 = moment(createdAt);
+console.log(date3.format('h:mm a')) // prints 1:00 am, which correspond to the time of the time stamp declared on line 28 above
+let timestamp2 = moment().valueOf() // This is moment() way of creating the syntax on line 6 above. This creates the timestamp of the current time and store it in timeStamp2 variable
+console.log(timestamp2) // prints the timestamp of the current time to the console
--- public/js/index.js
+++ public/js/index.js
@@ -15,21 +15,41 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);  
+    
+    // let formattedTime = moment(message.createdAt).format('h:mm a');
+    // let li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target=_blank>My current location</a>');
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from}: ${formattedTime} `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
+
+    // let formattedTime = moment(message.createdAt).format('h:mm a');
+    // let li = jQuery('<li></li>');
+    // let a = jQuery('<a target=_blank>My current location</a>');
+
+    // li.text(`${message.from}: ${formattedTime} `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
